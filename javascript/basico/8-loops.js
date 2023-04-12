040073db5cff69b87ecc92a48f2af5680943ba97
Peterson Torres <devpetrr@gmail.com>
2023-04-12 13:56:31 -0300

    initializing-DOM

--- javascript/basico/8-loops.js
+++ javascript/basico/8-loops.js
@@ -10,6 +10,7 @@ const idadeComprador = 16;
 const estaAcompanhada = true;
 let temPassagemComprada = false;
 const destino = "Salvador";
+const destino2 = "Sergipe";
 
 
 console.log("\nDestinos possíveis: ")
@@ -24,12 +25,24 @@ let i = 0;
 
 while (i<3) {
     if (listaDeDestinos[i] == destino) {
-      console.log("Destino existe!")
+      console.log("Destino existe!");
+      break;
     } else {
-      console.log("Destino não existe!")
+      console.log("Destino não existe!");
+      break;
     }
     i++
 }
 
 console.log(`\nLista de destinos atualizada: ${listaDeDestinos}\n`);
 
+
+for (let cont=0; cont<3; cont++) {
+  if (listaDeDestinos[i] == destino2) {
+    console.log("Destino existe!");
+    break;
+  } else {
+    console.log("Destino não existe!");
+    break;
+  }
+}
